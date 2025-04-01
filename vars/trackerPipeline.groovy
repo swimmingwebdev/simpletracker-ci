9fb173dc4137b27b35e9c1acb93b148161c7c86e
swimmingwebdev <swimmingwebdev@gmail.com>
2025-04-01 12:28:08 -0700

    update

--- vars/trackerPipeline.groovy
+++ vars/trackerPipeline.groovy
@@ -57,24 +57,25 @@ def call(dockerRepoName, imageName, portNum) {
                     withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
                         sh "echo $TOKEN | docker login -u swimminwebdev --password-stdin"
                         sh "docker build -f ${imageName}/Dockerfile -t swimminwebdev/${dockerRepoName}:latest -t swimminwebdev/${dockerRepoName}:${imageName} ${imageName}/"
+                        sh "docker push swimminwebdev/${dockerRepoName}:latest"
                         sh "docker push swimminwebdev/${dockerRepoName}:${imageName}"
                     }
                 }
             }
 
-            stage('Deploy') {
-                when {
-                    expression { params.DEPLOY }
-                }
-                steps {
-                    withCredentials([sshUserPrivateKey(credentialsId: 'ec2-ssh-key', keyFileVariable: 'KEY')]) {
-                        sh '''
-                            ssh -i "$KEY" -o StrictHostKeyChecking=no ubuntu@ec2-34-234-232-11.compute-1.amazonaws.com << "EOF"
-                            cd /home/ubuntu/simpletracker
-                            docker-compose pull ${dockerRepoName}
-                            docker-compose up -d ${dockerRepoName}
-                            EOF
-                        '''
+        stage('Deploy') {
+            when {
+                expression { params.DEPLOY }
+            }
+            steps {
+                withCredentials([sshUserPrivateKey(credentialsId: 'ec2-ssh-key', keyFileVariable: 'KEY')]) {
+                    sh '''
+                        ssh -i "$KEY" -o StrictHostKeyChecking=no ubuntu@ec2-34-234-232-11.compute-1.amazonaws.com <<EOF
+                        cd /home/ubuntu/simpletracker
+                        docker-compose pull ${dockerRepoName}
+                        docker-compose up -d ${dockerRepoName}
+                        EOF
+                    '''
                     }
                 }
             }
