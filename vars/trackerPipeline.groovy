bc0aabb812d8fce6574cf195d938d4b33dc9ce57
swimmingwebdev <swimmingwebdev@gmail.com>
2025-04-01 00:44:32 -0700

    modify Deploy stage

--- vars/trackerPipeline.groovy
+++ vars/trackerPipeline.groovy
@@ -67,15 +67,13 @@ def call(dockerRepoName, imageName, portNum) {
                     expression { params.DEPLOY }
                 }
                 steps {
-                    sshagent(credentials: ['jenkins-agent-key']) {
-                        sh """
-                            ssh -o StrictHostKeyChecking=no -i /home/azureuser/.ssh/jenkins_agent_key azureuser@172.210.180.227 '
-                                cd /home/azureuser/simpletracker &&
-                                docker-compose pull &&
-                                docker-compose up -d
-                            '
-                        """
-                    }
+                    sh """
+                        ssh -o StrictHostKeyChecking=no -i /home/azureuser/.ssh/jenkins_agent_key azureuser@172.210.180.227 '
+                            cd /home/azureuser/simpletracker &&
+                            docker-compose pull &&
+                            docker-compose up -d
+                        '
+                    """
                 }
             }
         }
