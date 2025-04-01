1008ccf5bd7ac6ecabf56534bd529f22be08d39d
swimmingwebdev <swimmingwebdev@gmail.com>
2025-04-01 00:47:00 -0700

    modify Deploy stage

--- vars/trackerPipeline.groovy
+++ vars/trackerPipeline.groovy
@@ -67,13 +67,13 @@ def call(dockerRepoName, imageName, portNum) {
                     expression { params.DEPLOY }
                 }
                 steps {
-                    sh """
-                        ssh -o StrictHostKeyChecking=no -i /home/azureuser/.ssh/jenkins_agent_key azureuser@172.210.180.227 '
+                    sh '''
+                        ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/jenkins_agent_key azureuser@172.210.180.227 '
                             cd /home/azureuser/simpletracker &&
                             docker-compose pull &&
                             docker-compose up -d
                         '
-                    """
+                    '''
                 }
             }
         }
